a1b8d8008573bd93e06d85e232316f4aec6b5597
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2025-02-04 19:44:35 +0530

    day-1 python -task

---
+++ Day-1/day-01.py
@@ -0,0 +1,86 @@
+
+#printing single number
+print(2)
+
+#printing multiple numbers
+print(2,3,4,5)
+
+#with out variables 
+print(2+7)
+
+print(9-4)
+
+print(8*7)
+
+print(6/3)
+
+#print(5/0); #we will get 
+print(4**3)
+
+#printing string varialble
+print("After ") 
+
+
+#using variable arthemetic operations
+
+numOne =23456789
+numTwo = 2578999
+print( numOne+ numTwo )
+print( numTwo - numOne)
+print( numOne * numTwo )
+print( numOne / numTwo )
+print( numOne // numTwo ) # it prints only integer value
+
+name= "vineeth kumar" #this is string
+
+print(name)
+
+# type method is used to find the which data type it is.
+print(type(name)) 
+
+# len() its prints the length of the string
+print(len(name)) 
+
+name = "mudham vineeth"
+
+print(name)
+
+
+print(len(name))
+
+# Below statement prints Index of String
+print(name[5])
+
+#prints index from reverse
+print(name[-12])
+
+#slicing
+#It is denoted by colon(:) is used to prints the sub string
+#
+print("Slicing") #mudham vineeth
+print(name[0:6]) #its print mudham
+
+print(name[4:6])# am
+
+print(name[5:])# m vineeth
+
+print(name[:5])  #mudha
+
+print(name[:]) #mudham vineeth
+
+print(name[8:100]) #its print 8 index to remaining string
+
+
+print("After Reverse Slicing")
+print(name[-10:-3]) #am vine
+
+print("revese order")
+print(name[-2:-9 : -1]) #teeniv
+
+#its skip 2 letters and print
+print(name[0 : : 2]) #mda iet
+
+print(name[-2: :-2])#
+
+
+
