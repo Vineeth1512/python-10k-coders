23aba27eac97c477d4c0b50492cd533ea587e620
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2025-02-06 11:02:54 +0530

    data types

---
+++ Day-2/day-02.py
@@ -0,0 +1,82 @@
+#comple numbers
+
+cmp1 = 5 + 3j
+cmp2 = 6 - 3j
+print(cmp1 + cmp2)
+
+print(cmp1 - cmp2)
+
+print(cmp1 * cmp2)
+
+print(cmp1 / cmp2)
+
+print(cmp1 ** cmp2)
+
+#print(cmp1 // cmp2)
+
+#print(cmp1 % cmp2)
+
+
+bool1 = True
+
+print(bool1)
+
+print(type(bool1))
+
+print(int(bool1))
+
+print(int(False))
+
+#Sequences
+#list
+list1 = [1,2,3,4,5-4,54 ,'Vineeth','a',[7,9,8]]
+
+print(list1)
+
+print(list1[0])
+
+print(list1[len(list1)-1])
+
+print(list1[-1])
+
+print("Printing......")
+print(list1[-1][2])
+
+print("Slicing")
+
+print(list1[0 :5])
+
+print(list1[0 : -1 :2])
+
+print(list1[-4:-8 :-1])
+
+
+#printing elements one by one
+for i in list1:
+    print(i)
+
+#tuple
+tuple1= (4,5,6,7,2,'vineeth',(5,6,7))
+
+for i in tuple1:
+    print(i)
+
+print("Tuble example")
+
+print(tuple1[0])
+
+print(tuple1[0 :5])
+
+print(tuple1[-2:-5:-1])
+
+#range
+
+print("Range")
+print(list(range(0,100)))
+
+print(list(range(100 ,0 ,-1)))
+
+print(list(range(100 ,0 ,-3)))
+
+ 
+ 
