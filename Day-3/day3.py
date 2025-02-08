88ca710088622efa5edb1e24933f861ce318e2c6
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2025-02-08 11:10:59 +0530

    day-3 and day-4

---
+++ Day-3/day3.py
@@ -0,0 +1,193 @@
+
+#Duplicates are not allowed 
+#Insertion order is not preserved
+#If we give True or 1 it will take based on order which one come firstly
+set1={2,4,5,7,22,67,True,1,"Vineeth","Kumar"}
+#print(set1)
+
+
+#we can have duplicate values but we cannot duplicates keys
+dict1={
+    1:'vineeth',4:"raviteja",3:"sairam"
+}
+
+#print(dict1)
+
+#print(dict1[1])
+#print(dict1[4])
+
+
+
+a1=0+0j
+
+#print(bool(a1))
+
+
+
+#num1 = int(input("Enter a Number : "))
+#num2 = int(input("ENter second number :"))
+
+#print(num1 + num2)
+
+
+#Convert int to remaing all data types
+
+number =121
+#int to int
+print(int(number))           #True
+print(float(number))        #True
+print(str(number))          #True
+#print(list(number))         #False
+#print(tuple(number))       #False
+
+#print(set(number))         #False
+
+#print(dict(number))        #False
+print(bool(number))         #True
+
+print(complex(number))      #True
+print(range(number))      #True
+
+
+
+# Convert Float to remaing all data types
+
+print("Convert FLoat to all Data types")
+
+number =12.5
+
+print(float(number))           #True
+print(float(number))        #True
+print(str(number))          #True
+#print(list(number))         #False
+#print(tuple(number))       #False
+
+#print(set(number))         #False
+
+#print(dict(number))        #False
+print(bool(number))         #True
+
+#print(float(range(number))) #False
+
+print(complex(number))      #True
+
+
+# Convert String to remaing all data types
+
+print("Convert String to all Data types")
+
+str ="vineeth"
+
+#print(int(str))           #False
+#print(float(str))        #False
+#print(str(str))          #False
+print(list(str))         #True
+print(tuple(str))       #True
+
+print(set(str))         #True
+
+#print(dict(str))        #False
+print(bool(str))         #True
+
+#print(range(str)) #False
+
+#print(complex(str))      #False
+
+
+# Convert List to remaing all data types
+
+print("Convert list to all Data types")
+
+list1 =[3,5,7,9,0,34,'vin',[5,7,8]]
+
+#print(int(list1))           #False
+#print(float(list1))        #False
+#print(str(list1))          #False
+print(list(list1))         #True
+print(tuple(list1))       #True
+
+#print(set(list1))         #False
+
+#print(dict(list1))        #False
+print(bool(list1))         #True
+
+#print(range(list1))      #False
+
+#print(complex(list1))      #False
+
+
+# Convert tuple to remaing all data types
+
+print("Convert tuple to all Data types")
+
+tup1 =(3,5,7,9,0,34,'vin',(5,7,8))
+
+#print(int(list1))           #False
+#print(float(list1))        #False
+#print(str(list1))          #False
+print(list(list1))         #True
+print(tuple(list1))       #True
+
+#print(set(list1))         #False
+
+#print(dict(list1))        #False
+print(bool(list1))         #True
+
+#print(range(list1))      #False
+
+#print(complex(list1))      #False
+
+
+
+
+# Convert set to remaing all data types
+
+print("Convert set to all Data types")
+
+tup1 ={3,5,7,9,0,34,'vin',(5,7,8)}
+
+#print(int(tup1))           #False
+#print(float(tup1))        #False
+#print(str(tup1))          #False
+print(list(tup1))         #True
+print(tuple(tup1))       #True
+
+print(set(tup1))         #False
+
+#print(dict(tup1))        #False
+print(bool(tup1))         #True
+
+#print(range(tup1))      #False
+
+#print(complex(tup1))      #False
+
+
+# Convert bool to remaing all data types
+
+print("Convert bool to all Data types")
+
+bool1=True
+
+print(int(bool1))           #True
+print(float(bool1))        #True
+#print(str(bool1))          #False
+#print(list(bool1))         #False
+#print(tuple(bool1))       #False
+
+#print(set(bool1))         #False
+
+#print(dict(bool1))        #False
+print(bool(bool1))         #True
+
+print(range(bool1))      #True
+
+print(complex(bool1))      #True
+
+
+
+
+
+
+
+
+
---
+++ Day-4/Operators.py
@@ -0,0 +1,124 @@
+
+#Operators
+#Arthematic Operators + ,-,*,/,//,
+
+NumOne = 14
+NumTwo = 4
+
+print(NumOne + NumTwo)
+
+print(NumOne - NumTwo)
+
+print(NumOne * NumTwo)
+
+print(NumOne / NumTwo)
+
+print(NumOne // NumTwo)
+
+print(NumOne ** NumTwo)
+
+
+print(NumOne % NumTwo)
+
+
+#Assignment Operators 
+print("Assignment operators")
+
+NumTwo+=10
+print(NumTwo)
+
+
+NumTwo-=10
+print(NumTwo)
+
+NumTwo*=10
+print(NumTwo)
+
+NumTwo/=10
+print(NumTwo)
+
+NumTwo//=10
+print(NumTwo)
+
+NumTwo**=10
+print(NumTwo)
+
+
+#Relational operators
+
+print(6 > 8)
+
+print(5 == 3)
+
+print(5 <= 10)
+
+print(3 >= 10)
+
+print(5 != 4)
+
+#logical Operators
+print("Logical Operators")
+
+
+print( 5 < 6  and 7 < 8)
+
+print(True and True)
+
+print(False and (True and False and True))
+
+print(4 and 5)
+
+print (6 and 4)
+
+print( 0 and 4)
+
+print('' and True)
+
+print(1 and [])
+
+print (-1 and -2)
+
+print("vineeth" and "kumar")
+
+#Or Gate 
+
+print("OR Gate")
+print( False or False)
+
+print(True or True)
+
+print( False or True)
+
+print(2 or 3)#2
+
+print( 3 or 2)#3
+
+print(0 or 2 )#2
+print(2 or 0) #2
+
+print('' or True)
+print(' ' or True)
+print(1 or [])
+
+print(-1 or -2)
+
+#Not Operators
+
+
+
+
+#Bitwise operators & | ^ ~ >> <<
+
+print(2 & 4)
+
+# 0010
+# 0100
+# 0000
+# (Or ) |
+print(13 | 15)
+# 1101
+# 1111
+# 1111
+print(13 ^ 15)
+# 0010
+
